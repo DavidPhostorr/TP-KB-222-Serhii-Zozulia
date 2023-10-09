ef6bd243bc1f49ad1f1fbcd8dfc1ce2bf4d5f96a
David <pysikmysik41@gmail.com>
2023-10-09 18:29:32 +0300

    fix task1

--- topic_04/task1.py
+++ topic_04/task1.py
@@ -41,9 +41,6 @@ while True:
         
         n1 = GetIntValue("Enter first number: ")
         n2 = GetIntValue("Enter second number: ")
-    
-    #    n1 = float(input("Enter first number: "))
-    #    n2 = float(input("Enter second number: "))
 
         
         if choiсe == '1':
