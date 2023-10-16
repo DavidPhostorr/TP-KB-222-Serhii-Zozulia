ca892c645fab363bafbb174433681ca08788d9a7
David <pysikmysik41@gmail.com>
2023-10-16 11:47:09 +0300

    Update task2.py

--- topic_05/task2.py
+++ topic_05/task2.py
@@ -7,12 +7,12 @@ data = response.json()
 rates = {item["cc"]: item["rate"] for item in data}
 while True:
     
-    amount = float(input("Enter amount of money(UAH): "))
+    amount = float(input("Enter amount of money: "))
     currency = input("Enter the code of the currency you want to transfer to:").upper()
 
     if currency not in rates:
         print("Currency not found")
     else: 
-        converted = amount / rates[currency]
-        print(f"{amount} UAH = {converted} {currency}")
+        converted = amount * rates[currency]
+        print(f"{amount} {currency} = {converted} UAH")
         
