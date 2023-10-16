9c9f96772cfd0db1fd134b581108cbdf1ac28dde
David <pysikmysik41@gmail.com>
2023-10-16 10:32:58 +0300

    topic_05

--- README.md
+++ README.md
@@ -5,12 +5,15 @@ add solution for task1 task2 and task3
 add folder topic_02
 add solution for task1 task2
 # 2023-10-03
-add folder topic_3
+add folder topic_03
 add solution for task1 task2 task3
 # 2023-10-04
 add folder lab_01
 add solution for lab_01
 # 2023-10-06
-add folder topic_02
+add folder topic_04
 add solution for task1 task2
+# 2023-10-13
+add folder topic_05
+add solution for task1 task2 task3
 # TP-KB-222-Serhii-Zozulia
--- topic_05/task2.py
+++ topic_05/task2.py
@@ -1,8 +1,18 @@
 import requests
 
-value = requests.get("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json")
+url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json"
+response = requests.get(url)
+data = response.json()
 
-for elem in value.json():
-    if elem['cc'] in ['EUR' , 'USD' , 'PLN']:
-        print(elem['cc'], "  ", elem['rate'])
+rates = {item["cc"]: item["rate"] for item in data}
+while True:
     
+    amount = float(input("Enter amount of money(UAH): "))
+    currency = input("Enter the code of the currency you want to transfer to:").upper()
+
+    if currency not in rates:
+        print("Currency not found")
+    else: 
+        converted = amount / rates[currency]
+        print(f"{amount} UAH = {converted} {currency}")
+        
